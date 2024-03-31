12191da4680667bbc55ac716aef30f63a9d3bb06
kennguyen887 <123860745+kennguyen887@users.noreply.github.com>
2024-03-31 13:40:56 +0700

    Update download_audio.py

--- download_audio.py
+++ download_audio.py
@@ -1,9 +1,14 @@
-# from pytube import Channel
+from pytube import Channel
+import os
 from pytube import YouTube
 from pytube import Playlist
+
+from datetime import datetime
+
+
     
 AUDIO_SAVE_DIRECTORY = "./download"
-YOUTUBE_PLAYPLIST_URL = "https://youtube...."
+YOUTUBE_PLAYPLIST_URL = "https://www.youtube.com/watch?v=NsMWTiwG97A&list=PLv9w1I38lMN5TK3RA_fKrPH91q4yQ1wbc"
 
 
 def download_audio(video_url):
@@ -11,16 +16,25 @@ def download_audio(video_url):
     audio = video.streams.filter(only_audio = True).first()
 
     try:
-        audio.download(YOUTUBE_PLAYPLIST_URL)
+        audio.download(AUDIO_SAVE_DIRECTORY)
     except:
         print("Failed to download audio")
 
     print("audio was downloaded successfully")
 
 
-p = Playlist()
+p = Playlist(YOUTUBE_PLAYPLIST_URL)
+
+
+def is_file_exists(folder_path, filename):
+    file_path = os.path.join(folder_path, filename)
+    return os.path.exists(file_path)
+
 
 
 for url in p.video_urls:
-    print(url)
-    download_audio(url)
+    video = YouTube(url)
+    if not is_file_exists(AUDIO_SAVE_DIRECTORY, f"{video.title.replace(".", "")}.mp4"):
+        print(f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} | Downloading video: {video.title}")
+        download_audio(url)
+        
